1fb8e6570275f40ff3a77835e50c0031d89d8257
cmwslw <cwalker32@gmail.com>
2013-04-27 14:17:35 -0400

    Travis testing.

--- selenium-crawler/makeparsed.py
+++ selenium-crawler/makeparsed.py
@@ -23,7 +23,7 @@ def parse_raw_script(name, directory, path):
     '''from selenium import webdriver
 
 def handle_link(link):
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome()
     driver.implicitly_wait(30)
     driver.get(link)
 
