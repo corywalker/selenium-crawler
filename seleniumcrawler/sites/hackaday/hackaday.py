e49df4c1f40a330af19d90b7c59d49ac1acee86c
cmwslw <cwalker32@gmail.com>
2013-04-30 02:31:28 -0400

    Fixed #1.

--- seleniumcrawler/sites/hackaday/hackaday.py
+++ seleniumcrawler/sites/hackaday/hackaday.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
+def get_profile():
+    # get the Firefox profile object
+    firefoxProfile = FirefoxProfile()
+    # Disable CSS
+    firefoxProfile.set_preference('permissions.default.stylesheet', 2)
+    # Disable images
+    firefoxProfile.set_preference('permissions.default.image', 2)
+    # Disable Flash
+    firefoxProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so',
+                                    'false')
+
+    return firefoxProfile
 
 def handle_link(link):
-    driver = webdriver.Firefox()
+    profile = get_profile()
+
+    # Set the modified profile while creating the browser object
+    driver = webdriver.Firefox(profile)
     driver.implicitly_wait(30)
     driver.get(link)
 
